153c0d64797a8590cd7b9708c67620cb76432d97
EriVelk <erve@live.com.mx>
2022-05-17 16:15:43 -0500

    first commit

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ProductList.css'
+import Product from '../product/Product';
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className='pl'>
+        <div className='pl-texts'>
+            <h1 className='pl-title'>Create & inspire. It's Veliz</h1>
+            <p className='pl-desc'>
+                Veliz is a creative portfolio that your word has been waiting for.
+                Beautiful homes, stunning portfolio styles & a whole lot more awaits inside.
+            </p>
+        </div>
+        <div className='pl-list'>
+            {products.map((item) =>(
+                <Product key={item.id} img={item.img} link={item.link} title={item.title}/>
+            ))
+
+            }
+        </div>
+    </div>
+  )
+}
+
+export default ProductList
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/1qWJ5gf/screencapture-library-nodeapp-herokuapp-catalog-2022-05-17-15-44-53.png",
+      link: "https://library-nodeapp.herokuapp.com/",
+      title:"Library APP"
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/WVsztNv/ecommerce.png",
+      link: "https://documenter.getpostman.com/view/10682643/UyxhonQP",
+      title:"Ecommerce API"
+    },
+    {
+      id: 3,
+      img: "",
+      link: "https://github.com/EriVelk",
+      title:"Medicament API"
+    },
+    {
+      id: 4,
+      img: "",
+      link: "https://github.com/EriVelk",
+      title:"Booking REACT"
+    },
+    {
+      id: 5,
+      img: "https://i.ibb.co/5hLQ8YR/maps.png",
+      link: "https://maps-react-sable.vercel.app/",
+      title:"Points REACT"
+    },
+    {
+      id: 6,
+      img: "",
+      link: "https://github.com/EriVelk",
+      title:"Library APP"
+    },
+  ];
