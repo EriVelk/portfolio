0525f166286dd84d5ad16e7ab84c914f5209d499
EriVelk <erve@live.com.mx>
2022-06-09 15:52:44 -0500

    Progess bar

--- src/components/progress/Progress.jsx
+++ src/components/progress/Progress.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 
 const Progress = () => {
     return (
-        <div className="p">
-            <div className='p-left'>
+        <div className="rog">
+            <div className='rog-left'>
                 <h3>Skills</h3>
             </div>
-            <div className='p-right'>
-                <div className='p-item'>
+            <div className='rog-right'>
+                <div className='rog-item'>
                 <p>JAVA</p>
                     <progress id="file" max="100" value="90"> 90% </progress>
                 </div>
                 <p>JavaScript</p>
-                <div className='p-item'>
+                <div className='rog-item'>
                     <progress id="file" max="100" value="80"> 80% </progress>
                 </div>
-                <div className='p-item'>
+                <div className='rog-item'>
                 <p>NodeJS</p>
                     <progress id="file" max="100" value="75"> 75% </progress>
                 </div>
-                <div className='p-item'>
+                <div className='rog-item'>
                 <p>MySQL</p>
                     <progress id="file" max="100" value="80"> 80% </progress>
                 </div>
-                <div className='p-item'>
+                <div className='rog-item'>
                 <p>MongoDB</p>
                     <progress id="file" max="100" value="70"> 70% </progress>
                 </div>
